297bf7bdb948d303fc6445d505f2e1243200ae1c
sonaPag <sdaccord@gmail.com>
2014-08-25 02:52:27 +0400

    добавила тестов

--- my_unittest.py
+++ my_unittest.py
@@ -10,22 +10,47 @@ class Test(unittest.TestCase):
     def test_basic_email_false(self):
         #check "@"
         self.assertFalse(check_email("usernamedomain.com"))
+    def test_double_email_false(self):
+        #check "@"
+        self.assertFalse(check_email("usernam@@edo@main.com"))
     def test_domain_true(self):
         #domain [a-z 0-9_-]
         self.assertTrue(check_email("username@do_main-007.com"))
+    def test_domain_false_symbol(self):
+        #domain [a-z 0-9_-]
+        self.assertFalse(check_email("username@do_main:007.com"))
+    def test_domain_dot_false(self):
+        #domain none dot
+        self.assertFalse(check_email("username@com"))
     def test_domain_false(self):
         #domain {3,256}
         self.assertFalse(check_email("username@co"))
+    def test_domain_false257(self):
+        #domain {3,256}
+        my_email = "username@" + 'a'*300+".com"
+        self.assertFalse(check_email(my_email))
     def test_domain_symbol(self):
         #domain [^-]
         self.assertFalse(check_email("username@-domain_007.com"))
     def test_domain_symbol_error(self):
         #domain [^-]
         self.assertFalse(check_email("username@domain_007-.com"))
+    def test_domain_symbol_error2(self):
+        #domain [^-]
+        self.assertFalse(check_email("username@domain_007.com-"))
+    def test_domain_space(self):
+        #space domain error
+        self.assertFalse(check_email("username@do  main.com"))
     def test_name_error(self):
         #name {128}
         my_email = 'a'*129 + "@domain.com"
         self.assertFalse(check_email(my_email))
+    def test_name_none(self):
+        #none name error
+        self.assertFalse(check_email("@domain.com"))
+    def test_name_space(self):
+        #space name error
+        self.assertFalse(check_email("a      @domain.com"))
     def test_name_double_dot(self):
         #double dot error
         self.assertFalse(check_email("user..name@domain.com"))
@@ -35,6 +60,9 @@ class Test(unittest.TestCase):
     def test_name_odd_quotes(self):
         #odd quotes error
         self.assertFalse(check_email('us"er"name"@domain.com'))
+    def test_name_odd_quotes2(self):
+        #odd quotes error
+        self.assertFalse(check_email('us"ername@domain.com'))
     def test_name_symbol1_check(self):
         # "!" rule
         self.assertTrue(check_email('username"!"@domain.com'))
