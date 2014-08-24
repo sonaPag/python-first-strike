075cabe86fb77473a238d43522bf79aba006ad54
sonaPag <sdaccord@gmail.com>
2014-08-25 03:30:11 +0400

    добавила тесты

--- my_unittest.py
+++ my_unittest.py
@@ -7,6 +7,9 @@ class Test(unittest.TestCase):
     def test_basic_email_true(self):
         #check "@"
         self.assertTrue(check_email("username@domain.com"))
+    def test_basic_email_true(self):
+        #check "."
+        self.assertTrue(check_email("username@do.main.com"))
     def test_basic_email_false(self):
         #check "@"
         self.assertFalse(check_email("usernamedomain.com"))
@@ -41,6 +44,15 @@ class Test(unittest.TestCase):
     def test_domain_space(self):
         #space domain error
         self.assertFalse(check_email("username@do  main.com"))
+    def test_domain__dot2(self):
+        #dot error
+        self.assertFalse(check_email("username@domcom."))
+    def test_domain__dot3(self):
+        #dot error
+        self.assertFalse(check_email("username@dom..com"))
+    def test_domain__dot4(self):
+        #dot error
+        self.assertFalse(check_email("username@.domcom."))
     def test_name_error(self):
         #name {128}
         my_email = 'a'*129 + "@domain.com"
@@ -54,6 +66,9 @@ class Test(unittest.TestCase):
     def test_name_double_dot(self):
         #double dot error
         self.assertFalse(check_email("user..name@domain.com"))
+    def test_name_double_dot2(self):
+        #dot error
+        self.assertFalse(check_email("username@domcom."))
     def test_name_even_quotes(self):
         #even quotes
         self.assertTrue(check_email('"us"er"name"@domain.com'))
@@ -78,9 +93,15 @@ class Test(unittest.TestCase):
     def test_name_symbol3_check(self):
         # ":" rule
         self.assertTrue(check_email('us":"ername@domain.com'))
+    def test_name_symbol3_check2(self):
+        # ":" rule
+        self.assertFalse(check_email('us":er"name@domain.com'))
     def test_name_symbol3_false(self):
         # ":" rule
         self.assertFalse(check_email('us:ername@domain.com'))
+    def test_name_uppercase_false(self):
+        # Uppercase rule
+        self.assertFalse(check_email('usAername@domain.com'))
 
 
 if __name__ == '__main__':
