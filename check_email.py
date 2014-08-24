e459ae5bbe7a85bab1ade052a1b896a75777312b
sonaPag <sdaccord@gmail.com>
2014-08-25 02:51:34 +0400

    поправила ошибки

--- check_email.py
+++ check_email.py
@@ -12,19 +12,22 @@ def check_email(email):
     '''
 
     my_email = re.split(r'\@',email)
-    if len(my_email) != 2:
+    if len(my_email) != 2 or my_email[0] == "" or my_email == "":
         return False
 
     name  = my_email[0]
     domains = my_email[1]
 
-    if re.search(r'^-|[^a-zA-Z0-9\_\-\.]|-\.|\.-|.{257}|^.{0,2}$|-$', domains) is not None:
+    if re.search(r'^-|[^a-zA-Z0-9\_\-\.]|-\.|\.-|.{257}|\s|^.{0,2}$|-$', domains) is not None:
+        return False
+
+    if re.search(r'\.', domains) is None:
         return False
 
     if len(re.findall(r'(")', name)) % 2 != 0:
         return False
 
-    if re.search(r'(\.\.)|.{129}|[^!:,a-z0-9"\._-]|(^|[^"])[!:,]"|"[!:,]([^"]|$)|(^|[^"])[!:,]([^"]|$)', name) is not None:
+    if re.search(r'(\.\.)|.{129}|[^!:,a-z0-9"\._-]|\s|(^|[^"])[!:,]"|"[!:,]([^"]|$)|(^|[^"])[!:,]([^"]|$)', name) is not None:
         return False
 
     return True
